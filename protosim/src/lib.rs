b95746cd4e4fd02cd43645a88a033572ff2dccb2
kayibal <alan@datarevenue.com>
2022-11-25 23:38:03 +0100

    Initial commit

---
+++ protosim/src/lib.rs
@@ -0,0 +1,17 @@
+pub fn birth() {
+    let random_quote = "The way I see it, there's only three kinds of people in this world: \
+        Bad ones, ones you follow, and ones you need to protect.\n\
+        \n\
+        Amos Burton";
+    println!("{}", random_quote)
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_birth() {
+        birth();
+    }
+}
