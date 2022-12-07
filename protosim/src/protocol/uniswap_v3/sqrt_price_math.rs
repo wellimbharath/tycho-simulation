d0f1ad27368ad58011fdb47f3f9a7437f291029e
kayibal <alan@datarevenue.com>
2022-12-07 21:29:11 +0000

    UniswapV3: SqrtPriceMath add tests for get_amount0_delta

--- protosim/src/protocol/uniswap_v3/sqrt_price_math.rs
+++ protosim/src/protocol/uniswap_v3/sqrt_price_math.rs
@@ -1,5 +1,6 @@
 use super::solidity_math::mul_div_rounding_up;
-use ethers::types::{U256, U512};
+use ethers::types::U256;
+const Q96: &str = "79228162514264337593543950336";
 
 fn maybe_flip_ratios(a: U256, b: U256) -> (U256, U256) {
     if a > b {
@@ -18,23 +19,89 @@ fn div_rounding_up(a: U256, b: U256) -> U256 {
     }
 }
 
-fn get_amount0_delta(a: U256, b: U256, liquidity: U256, round_up: bool) -> U256 {
+pub fn get_amount0_delta(a: U256, b: U256, liquidity: U256, round_up: bool) -> U256 {
     let (sqrt_ratio_a, sqrt_ratio_b) = maybe_flip_ratios(a, b);
 
-    let numerator1 = liquidity << 96;
-    let numerator2 = b - a;
+    let numerator1 = liquidity << U256::from(96);
+    let numerator2 = sqrt_ratio_b - sqrt_ratio_a;
 
     if sqrt_ratio_a <= U256::zero() {
         // TODO raise an error
     }
-    let res;
+
     if round_up {
-        res = div_rounding_up(
+        div_rounding_up(
             mul_div_rounding_up(numerator1, numerator2, sqrt_ratio_b),
             sqrt_ratio_a,
-        );
+        )
     } else {
-        res = mul_div_rounding_up(numerator1, numerator2, sqrt_ratio_b);
+        mul_div_rounding_up(numerator1, numerator2, sqrt_ratio_b) / sqrt_ratio_a
     }
-    return res;
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    struct TestCase {
+        args: (U256, U256, U256, bool),
+        exp: U256,
+    }
+
+    #[test]
+    fn test_maybe_flip() {
+        let a = U256::from_dec_str("646922711029656030980122427077").unwrap();
+        let b = U256::from_dec_str("78833030112140176575862854579").unwrap();
+        let (a1, b1) = maybe_flip_ratios(a, b);
+
+        assert_eq!(b, a1);
+        assert_eq!(a, b1);
+    }
+
+    #[test]
+    fn test_get_amount_0_delta() {
+        let cases = vec![
+            TestCase {
+                args: (
+                    U256::from_dec_str("646922711029656030980122427077").unwrap(),
+                    U256::from_dec_str("78833030112140176575862854579").unwrap(),
+                    U256::from_dec_str("1000000000000").unwrap(),
+                    true,
+                ),
+                exp: U256::from_dec_str("882542983628").unwrap(),
+            },
+            TestCase {
+                args: (
+                    U256::from_dec_str("646922711029656030980122427077").unwrap(),
+                    U256::from_dec_str("78833030112140176575862854579").unwrap(),
+                    U256::from_dec_str("1000000000000").unwrap(),
+                    false,
+                ),
+                exp: U256::from_dec_str("882542983627").unwrap(),
+            },
+            TestCase {
+                args: (
+                    U256::from_dec_str("79224201403219477170569942574").unwrap(),
+                    U256::from_dec_str("79394708140106462983274643745").unwrap(),
+                    U256::from_dec_str("10000000").unwrap(),
+                    true,
+                ),
+                exp: U256::from_dec_str("21477").unwrap(),
+            },
+            TestCase {
+                args: (
+                    U256::from_dec_str("79224201403219477170569942574").unwrap(),
+                    U256::from_dec_str("79394708140106462983274643745").unwrap(),
+                    U256::from_dec_str("10000000").unwrap(),
+                    false,
+                ),
+                exp: U256::from_dec_str("21476").unwrap(),
+            },
+        ];
+
+        for case in cases {
+            let res = get_amount0_delta(case.args.0, case.args.1, case.args.2, case.args.3);
+            assert_eq!(res, case.exp);
+        }
+    }
 }
