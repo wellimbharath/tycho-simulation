efbf1342541475b606bbb25cf2841b91813eb80b
czanella <carlos@datarevenue.com>
2022-12-12 12:02:10 -0300

    Continue implementing tick list
    - add tick_idxs attribute for easy checking positioning
    - implement push properly
    - return nice errors for valid_ticks function
    - add tests for the above

--- protosim/src/protocol/uniswap_v3/tick_list.rs
+++ protosim/src/protocol/uniswap_v3/tick_list.rs
@@ -18,6 +18,7 @@ impl PartialOrd for TickInfo {
 pub struct TickList {
     tick_spacing: usize,
     ticks: Vec<TickInfo>,
+    tick_idxs: Vec<usize>,
 }
 
 impl TickList {
@@ -25,50 +26,87 @@ impl TickList {
         return TickList {
             tick_spacing: spacing,
             ticks: Vec::with_capacity(20),
+            tick_idxs: Vec::with_capacity(20),
         };
     }
 
     pub fn from(spacing: usize, ticks: Vec<TickInfo>) -> Self {
+        let tick_idxs = ticks.iter().map(|&x| x.index).collect();
         let tick_list = TickList {
             tick_spacing: spacing,
             ticks: ticks,
+            tick_idxs: tick_idxs,
         };
-        if tick_list.valid_ticks() {
+        let valid = tick_list.valid_ticks();
+        if valid.is_ok() {
             return tick_list;
         } else {
-            panic!()
+            panic!("{}", valid.unwrap_err());
         }
     }
 
-    fn valid_ticks(&self) -> bool {
+    // Asserts that all attributes are valid. Checks for:
+    // 1. Tick spacing > 0
+    // 2. Tick indexes have no rest when divided by tick spacing
+    // 3. Ticks are ordered by index
+    // TODO: test
+    fn valid_ticks(&self) -> Result<bool, String> {
         if self.tick_spacing == 0 {
-            return false;
+            return Err(String::from("Tick spacing is 0"));
         }
 
         for i in 0..self.ticks.len() {
             let t = self.ticks.get(i).unwrap();
             if t.index % self.tick_spacing != 0 {
-                return false;
+                return Err(format!(
+                    "Tick index {} not aligned with tick spacing",
+                    t.index
+                ));
             }
             if i != self.ticks.len() && t > self.ticks.get(i + 1).unwrap() {
-                return false;
+                let msg = format!("Ticks are not ordered at position {}", t.index);
+                return Err(msg);
             }
         }
 
-        return true;
+        return Ok(true);
     }
 
     pub fn push(&mut self, tick: TickInfo) {
-        self.ticks.push(tick);
-        self.ticks.insert(tick.index, tick);
-        self.ticks.sort_by_key(|t| t.index);
+        let index_to_push = self.tick_idxs.binary_search(&tick.index);
+        if index_to_push.is_ok() {
+            panic!("Tick at index {} already exists!", tick.index)
+        }
+        
+        self.ticks.insert(index_to_push.unwrap_err(), tick);
+        self.tick_idxs.insert(index_to_push.unwrap_err(), tick.index);
+        let valid = self.valid_ticks();
+        if valid.is_err() {
+            panic!("{}", valid.unwrap_err());
+        }
     }
 
-    // TODO: which one is faster heap reference or copy to stack?
-    pub fn is_below_smallest(&mut self, tick: usize) -> bool {
+    // TODO: test
+    pub fn is_below_smallest(&self, tick: usize) -> bool {
         let t = &self.ticks[tick];
         return tick < t.index;
     }
+
+    // TODO: test
+    pub fn is_below_safe_tick(&self, tick: usize) -> bool {
+        let smallest = &self.ticks[0];
+        let minimum = smallest.index - self.tick_spacing;
+        return tick < minimum;
+    }
+
+    // def is_at_or_above_largest(self, tick: int) -> bool:
+    // assert len(self.ticks), "LENGTH"
+    // return tick >= self.ticks[-1].tick_idx
+
+    // def is_at_or_above_safe_tick(self, tick: int) -> bool:
+    //     largest = self.ticks[-1].tick_idx
+    //     maximum = largest + self.tick_spacing
+    //     return tick >= maximum
 }
 
 #[cfg(test)]
@@ -98,6 +136,7 @@ mod tests {
         let tick_list = create_tick_list();
         assert_eq!(tick_list.ticks.len(), 3);
         assert_eq!(tick_list.tick_spacing, 10);
+        assert_eq!(tick_list.ticks.capacity(), 20);
     }
 
     #[test]
@@ -108,10 +147,17 @@ mod tests {
     }
 
     #[test]
+    #[should_panic]
     fn test_push_tick_duplicate_ix() {
         let mut tick_list = create_tick_list();
         tick_list.push(create_tick_info(30, 10));
-        assert_eq!(tick_list.ticks.len(), 3);
+    }
+
+    #[test]
+    #[should_panic]
+    fn test_push_tick_invalid_ix() {
+        let mut tick_list = create_tick_list();
+        tick_list.push(create_tick_info(35, 10));
     }
 
     #[test]
