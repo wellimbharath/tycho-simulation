8842aafdf9542b3cd97a3776a7ce91a2f438ac65
kayibal <alan@datarevenue.com>
2022-12-12 19:58:20 +0100

    UniswapV3: WIP swap method

--- protosim/src/protocol/uniswap_v3/mod.rs
+++ protosim/src/protocol/uniswap_v3/mod.rs
@@ -1,7 +1,7 @@
 mod liquidity_math;
 mod solidity_math;
 mod sqrt_price_math;
-mod state;
+pub mod state;
 mod swap_math;
 mod tick_list;
 mod tick_math;
--- protosim/src/protocol/uniswap_v3/state.rs
+++ protosim/src/protocol/uniswap_v3/state.rs
@@ -1,8 +1,77 @@
-use ethers::types::{I256, U256, U512};
+use ethers::types::{I256, U256};
 
-const UINT160_MAX: &str = "1461501637330902918203684832716283019655932542975";
+use crate::models::ERC20Token;
 
-const Q192: &str = "6277101735386680763835789423207666416102355444464034512896";
-const MAX_FEE: u64 = 1_000_000;
+use super::tick_math;
 
-pub struct UniswapV3State {}
+pub struct TradeSimulationError {}
+
+pub struct UniswapV3State {
+    liquidity: U256,
+    sqrt_price: U256,
+    fee: u32,
+    tick: i32,
+    current_pos: usize,
+}
+
+struct SwapState {
+    amount_remaining: I256,
+    amount_calulated: I256,
+    sqrt_price: U256,
+    tick: i32,
+    liquidity: U256,
+}
+
+struct StepComputation {
+    sqrt_price_start: U256,
+    amount_calculated: I256,
+    sqrt_price: U256,
+    tick: i32,
+    liquidity: U256,
+}
+
+impl UniswapV3State {
+    pub fn get_amount_out(
+        &self,
+        amount_in: U256,
+        token_a: &ERC20Token,
+        token_b: &ERC20Token,
+    ) -> Result<U256, TradeSimulationError> {
+        let zero_for_one = token_a < token_b;
+
+        Ok(U256::zero())
+    }
+
+    fn swap(&self, zero_for_one: bool, amount_specified: I256, sqrt_price_limit: Option<U256>) {
+        let price_limit = if sqrt_price_limit.is_none() {
+            if zero_for_one {
+                tick_math::MIN_SQRT_RATIO + 1
+            } else {
+                tick_math::MAX_SQRT_RATIO - 1
+            }
+        } else {
+            sqrt_price_limit.unwrap()
+        };
+
+        if zero_for_one {
+            assert!(price_limit > tick_math::MIN_SQRT_RATIO);
+            assert!(price_limit < self.sqrt_price);
+        } else {
+            assert!(price_limit < tick_math::MAX_SQRT_RATIO);
+            assert!(price_limit > self.sqrt_price);
+        }
+
+        let exact_input = amount_specified > I256::zero();
+
+        let state = SwapState {
+            amount_remaining: amount_specified,
+            amount_calulated: I256::zero(),
+            sqrt_price: self.sqrt_price,
+            tick: self.tick,
+            liquidity: self.liquidity,
+        };
+        let n_ticks_crossed = 0u32;
+
+        while state.amount_remaining != I256::zero() && state.sqrt_price != price_limit {}
+    }
+}
