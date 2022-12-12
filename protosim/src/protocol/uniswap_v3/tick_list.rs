3a179cbe4a08dc40706c5a063cbe28183323a0ff
kayibal <alan@datarevenue.com>
2022-12-12 23:40:55 +0100

    Remove unnecessary tick index vector

--- protosim/src/protocol/uniswap_v3/tick_list.rs
+++ protosim/src/protocol/uniswap_v3/tick_list.rs
@@ -1,10 +1,12 @@
+use std::cmp::Ordering;
+
 use ethers::types::U256;
 
 // enum TickSpacing{one: 1, 100, 300, 5000}
 
-#[derive(Copy, Clone, PartialEq)]
+#[derive(Copy, Clone, PartialEq, Debug)]
 pub struct TickInfo {
-    index: usize,
+    index: i32,
     net_liquidity: U256,
     sqrt_price: U256,
 }
@@ -18,7 +20,6 @@ impl PartialOrd for TickInfo {
 pub struct TickList {
     tick_spacing: usize,
     ticks: Vec<TickInfo>,
-    tick_idxs: Vec<usize>,
 }
 
 impl TickList {
@@ -26,16 +27,13 @@ impl TickList {
         return TickList {
             tick_spacing: spacing,
             ticks: Vec::with_capacity(20),
-            tick_idxs: Vec::with_capacity(20),
         };
     }
 
     pub fn from(spacing: usize, ticks: Vec<TickInfo>) -> Self {
-        let tick_idxs = ticks.iter().map(|&x| x.index).collect();
         let tick_list = TickList {
             tick_spacing: spacing,
             ticks: ticks,
-            tick_idxs: tick_idxs,
         };
         let valid = tick_list.valid_ticks();
         if valid.is_ok() {
@@ -57,12 +55,15 @@ impl TickList {
 
         for i in 0..self.ticks.len() {
             let t = self.ticks.get(i).unwrap();
-            if t.index % self.tick_spacing != 0 {
+            if t.index % self.tick_spacing as i32 != 0 {
                 return Err(format!(
                     "Tick index {} not aligned with tick spacing",
                     t.index
                 ));
             }
+        }
+        for i in 0..self.ticks.len() - 1 {
+            let t = self.ticks.get(i).unwrap();
             if i != self.ticks.len() && t > self.ticks.get(i + 1).unwrap() {
                 let msg = format!("Ticks are not ordered at position {}", t.index);
                 return Err(msg);
@@ -73,30 +74,17 @@ impl TickList {
     }
 
     pub fn push(&mut self, tick: TickInfo) {
-        let index_to_push = self.tick_idxs.binary_search(&tick.index);
+        // let tickcmp_f = |el: &TickInfo| -> Ordering { el.index.cmp(&tick.index) };
+        let index_to_push = self.ticks.binary_search_by(|el| el.index.cmp(&tick.index));
         if index_to_push.is_ok() {
             panic!("Tick at index {} already exists!", tick.index)
         }
-        
+
         self.ticks.insert(index_to_push.unwrap_err(), tick);
-        self.tick_idxs.insert(index_to_push.unwrap_err(), tick.index);
         let valid = self.valid_ticks();
         if valid.is_err() {
             panic!("{}", valid.unwrap_err());
         }
-    }
-
-    // TODO: test
-    pub fn is_below_smallest(&self, tick: usize) -> bool {
-        let t = &self.ticks[tick];
-        return tick < t.index;
-    }
-
-    // TODO: test
-    pub fn is_below_safe_tick(&self, tick: usize) -> bool {
-        let smallest = &self.ticks[0];
-        let minimum = smallest.index - self.tick_spacing;
-        return tick < minimum;
     }
 
     // def is_at_or_above_largest(self, tick: int) -> bool:
@@ -115,15 +103,16 @@ mod tests {
     use super::*;
 
     fn create_tick_list() -> TickList {
-        let mut tick_list = TickList::new(10);
-        let liquidities = [(10, 10), (20, 5), (30, 0)];
-        for (ix, liq) in liquidities {
-            tick_list.push(create_tick_info(ix, liq));
-        }
-        return tick_list;
+        let tick_infos = vec![
+            create_tick_info(10, 10),
+            create_tick_info(20, 5),
+            create_tick_info(40, 0),
+        ];
+
+        TickList::from(10, tick_infos)
     }
 
-    fn create_tick_info(idx: usize, liq: u8) -> TickInfo {
+    fn create_tick_info(idx: i32, liq: u8) -> TickInfo {
         return TickInfo {
             index: idx,
             net_liquidity: U256::from(liq),
@@ -132,25 +121,41 @@ mod tests {
     }
 
     #[test]
-    fn test_new() {
+    fn test_from() {
         let tick_list = create_tick_list();
         assert_eq!(tick_list.ticks.len(), 3);
         assert_eq!(tick_list.tick_spacing, 10);
-        assert_eq!(tick_list.ticks.capacity(), 20);
     }
 
     #[test]
-    fn test_push_new_tick() {
+    fn test_push_new_tick_start() {
         let mut tick_list = create_tick_list();
-        tick_list.push(create_tick_info(40, 10));
+        tick_list.push(create_tick_info(-10, 10));
         assert_eq!(tick_list.ticks.len(), 4);
+        assert_eq!(tick_list.ticks[0].index, -10)
+    }
+
+    #[test]
+    fn test_push_new_tick_mid() {
+        let mut tick_list = create_tick_list();
+        tick_list.push(create_tick_info(30, 10));
+        assert_eq!(tick_list.ticks.len(), 4);
+        assert_eq!(tick_list.ticks[2].index, 30)
+    }
+
+    #[test]
+    fn test_push_new_tick_end() {
+        let mut tick_list = create_tick_list();
+        tick_list.push(create_tick_info(50, 10));
+        assert_eq!(tick_list.ticks.len(), 4);
+        assert_eq!(tick_list.ticks[3].index, 50)
     }
 
     #[test]
     #[should_panic]
     fn test_push_tick_duplicate_ix() {
         let mut tick_list = create_tick_list();
-        tick_list.push(create_tick_info(30, 10));
+        tick_list.push(create_tick_info(40, 10));
     }
 
     #[test]
