d0acd843cc916aff9f3881a9559969fe643a453e
kayibal <alan@datarevenue.com>
2022-12-22 17:43:04 +0100

    Q96 Price: Convert in sqrt domain to avoid overflows

--- protosim/src/protocol/uniswap_v3/q96_math.rs
+++ protosim/src/protocol/uniswap_v3/q96_math.rs
@@ -15,20 +15,16 @@ const MAX_U160: U256 = U256([0, 0, 4294967296, 0]);
 pub fn sqrt_price_q96_to_f64(x: U256, token_0_decimals: u32, token_1_decimals: u32) -> f64 {
     assert!(x <= MAX_U160);
     let token_correction = 10f64.powi(token_0_decimals as i32 - token_1_decimals as i32);
-    let price = U512::from(x).pow(U512::from(2));
 
-    // shift right so the whole 64 numerator bits are filled - maximizing precision
-    // note that we loose precision with a right shift
-    let price_bits = price.bits();
-    // theoretically price bits can be between 0 and 256...
-    // if it is between 0 and 64 we don't have to compress things
-    // we can simply divide by 2e192f64
-    // TODO: should we short circuit if price_bits <= 64?
-    // Currently we would probably inflate denom pow if it was < 64 unecessarily...
-    let shr_b = price_bits - 64;
-    if shr_b < 192 {
+    // if x.bits() < 128 it's squre will not exceed 256
+    if x.bits() < 128 {
         // price is < 1.0, we assume nomin < denom
         // we use the formula price = (x^2) >> shr_b /(2^(192 - shr_b))
+        // TODO: Should we do this in sqrt space?
+        // sqrt(price) = x / 2**96 = x >> shr_b / 2^(96 - shr_b)
+        let price = U512::from(x).pow(U512::from(2));
+        let price_bits = price.bits();
+        let shr_b = price_bits - 64;
         let denom_pow = 192 - shr_b;
 
         let res = (price >> shr_b).as_u64() as f64;
@@ -36,18 +32,18 @@ pub fn sqrt_price_q96_to_f64(x: U256, token_0_decimals: u32, token_1_decimals: u
         res as f64 / denom * token_correction
     } else {
         // price >= 1.0, we assume nomin >= denom
-        // we use the formula price = 2^2 * (x/2)^2 / 2^192
-        // assuming price uses 320 bits we would need to do a
-        // right shift by 320 - 64 = 256 bits we would need to
+        // in this case above method won't work consider price uses 320 bits (=160*2)
+        // we would need to do a right shift by 320 - 64 = 256 bits we would need to
         // do the same for denom which would result in 0
-        // We need to make the nominator use <= 32 bits then we can use previous formula and multiply the result with a power of 2
-        // so if we use 320 bits, we shift right by 288 and then square now we should use at most 64 bits
-        // the shift by 288 we have to convert into an additional factor price = x^2 = (2^288 * x/2^288)^2 = 2^288^2 * (x/2^288)^2
+        // we instead will reduce the nominator in sqrt space and only square in the end
+        // To reduce the nominator we do a right shift and introduce a power of two factor
+        // price = sqrt(price)^2
+        // sqrt(price) = x / 2^96 = 2 ^ shr_b * (x >> shr_b) / 2^96
         let x_bits = x.bits();
         let shr_b2 = x_bits - 64;
         let factor = 2f64.powi(shr_b2 as i32);
         let nomin = (x >> shr_b2).as_u64() as f64;
-        (factor * (nomin / 2f64.powi(96))).powi(2)
+        (factor * (nomin / 2f64.powi(96))).powi(2) * token_correction
     }
 }
 
@@ -56,7 +52,6 @@ mod tests {
     use approx::{assert_ulps_eq, relative_eq, ulps_eq};
 
     use super::*;
-    // TODO test case were precision is negative
 
     struct TestCase {
         sqrt_price: U256,
