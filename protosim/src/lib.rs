55096737ca333a1a048cad34e028658a7bc34a6e
kayibal <alan@datarevenue.com>
2023-01-04 17:40:57 +0100

    Remove unused code

--- protosim/src/lib.rs
+++ protosim/src/lib.rs
@@ -2,21 +2,3 @@ pub mod graph;
 pub mod models;
 pub mod protocol;
 mod u256_num;
-
-pub fn birth() {
-    let random_quote = "The way I see it, there's only three kinds of people in this world: \
-        Bad ones, ones you follow, and ones you need to protect.\n\
-        \n\
-        Amos Burton";
-    println!("{}", random_quote)
-}
-
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn test_birth() {
-        birth();
-    }
-}
