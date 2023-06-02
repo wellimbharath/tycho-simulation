b27470ca3cf558f5ed815485e0b56f1647732651
Michał D <dankiewicz.michal@gmail.com>
2023-06-02 13:27:48 +0200

    Clean up and adapt after merging main
    --- don't change below this line ---
    ROC-921 #time 1h 37m
    
    #time 49m

--- protosim-py/src/lib.rs
+++ protosim-py/src/lib.rs
@@ -1,29 +1,11 @@
-pub mod simulation_py;
-use ethers::providers::{Http, Provider};
-use ethers::types::{Address, Bytes, U256};
-use once_cell::sync::Lazy;
-use protosim::evm_simulation::simulation;
-use protosim::evm_simulation::{database::SimulationDB, simulation::SimulationEngine};
 use pyo3::prelude::*;
 use simulation_py::WrappedSimulationEnginePy;
-use std::collections::HashMap;
-use std::str::FromStr;
-use std::sync::Arc;
-use std::sync::Mutex;
-use tokio::runtime::{Handle, Runtime};
 
-/// Formats the sum of two numbers as string.
-#[pyfunction]
-fn sum_as_string(a: usize, b: usize) -> PyResult<String> {
-    println!("Call function");
-    Ok((a + b).to_string())
-}
+mod simulation_py;
 
 /// A Python module implemented in Rust.
 #[pymodule]
 fn protosim_py(_py: Python, m: &PyModule) -> PyResult<()> {
-    println!("Init Module");
-    m.add_function(wrap_pyfunction!(sum_as_string, m)?)?;
     m.add_class::<WrappedSimulationEnginePy>()?;
     Ok(())
 }
--- protosim-py/src/simulation_py.rs
+++ protosim-py/src/simulation_py.rs
@@ -2,18 +2,15 @@ use ethers::{
     providers::{Http, Provider},
     types::{Address, Bytes, U256},
 };
-use protosim::evm_simulation::{
-    account_storage::StateUpdate,
-    database::SimulationDB,
-    simulation::{self, SimulationEngine},
-};
+use protosim::evm_simulation::{account_storage::StateUpdate, database::SimulationDB, simulation::{self, SimulationEngine}};
 use pyo3::prelude::*;
-use revm::primitives::hash_map;
-use std::{collections::HashMap, str::FromStr, sync::Arc};
+use revm::precompile::HashMap as revmHashMap;
+use std::{collections::HashMap as stdHashMap, str::FromStr, sync::Arc};
 use tokio::runtime::Runtime;
+use protosim::evm_simulation::simulation::{SimulationParameters, SimulationResult};
 
-#[derive(FromPyObject, Clone, Debug)]
 /// Data needed to invoke a transaction simulation
+#[derive(FromPyObject, Clone, Debug)]
 pub struct PySimulationParameters {
     /// Address of the sending account
     pub caller: String,
@@ -25,42 +22,97 @@ pub struct PySimulationParameters {
     pub value: String,
     /// EVM state overrides.
     /// Will be merged with existing state. Will take effect only for current simulation.
-    pub overrides: Option<HashMap<String, String>>,
+    pub overrides: Option<stdHashMap<String, stdHashMap<String, String>>>,
     /// Limit of gas to be used by the transaction
     pub gas_limit: Option<u64>,
+}
+
+impl From<PySimulationParameters> for SimulationParameters {
+    fn from(params: PySimulationParameters) -> Self {
+        let overrides = match params.overrides {
+            Some(py_overrides) => {
+                let mut rust_overrides: revmHashMap<Address, revmHashMap<U256, U256>> = revmHashMap::new();
+                for (address, py_slots) in py_overrides {
+                    let mut rust_slots = revmHashMap::new();
+                    for (index, value) in py_slots {
+                        rust_slots.insert(
+                            U256::from_str(index.as_str())
+                                .expect("Can't decode storage slot index"),
+                            U256::from_str(value.as_str())
+                                .expect("Can't decode storage value"),
+                        );
+                    }
+                    rust_overrides.insert(
+                        Address::from_str(address.as_str())
+                            .expect("Wrong address format"),
+                        rust_slots,
+                    );
+                }
+                Some(rust_overrides)
+            },
+            None => None,
+        };
+        SimulationParameters {
+            caller: Address::from_str(params.caller.as_str()).unwrap(),
+            to: Address::from_str(params.to.as_str()).unwrap(),
+            data: Bytes::from(params.data),
+            value: U256::from_str(params.value.as_str()).unwrap(),
+            overrides,
+            gas_limit: params.gas_limit,
+        }
+    }
 }
 
 #[pyclass]
 #[derive(Clone, Debug)]
 pub struct PyStateUpdate {
-    pub storage: Option<HashMap<String, String>>,
+    pub storage: Option<stdHashMap<String, String>>,
     pub balance: Option<String>,
 }
 
-impl PyStateUpdate {
-    fn new(state_update: StateUpdate) -> Self {
-        let mut storage = HashMap::new();
+impl From<StateUpdate> for PyStateUpdate {
+    fn from(state_update: StateUpdate) -> Self {
+        let mut py_storage = stdHashMap::new();
         if let Some(rust_storage) = state_update.storage {
             for (key, val) in rust_storage {
-                storage.insert(key.to_string(), val.to_string());
+                py_storage.insert(key.to_string(), val.to_string());
             }
         }
 
         PyStateUpdate {
-            storage: Some(storage),
+            storage: Some(py_storage),
             balance: Some(state_update.balance.unwrap().to_string()),
         }
     }
 }
+
 #[pyclass]
 #[derive(Clone)]
 pub struct PySimulationResult {
     /// Output of transaction execution as bytes
     pub result: Vec<u8>,
     /// State changes caused by the transaction
-    pub state_updates: HashMap<String, PyStateUpdate>,
+    pub state_updates: stdHashMap<String, PyStateUpdate>,
     /// Gas used by the transaction (already reduced by the refunded gas)
     pub gas_used: u64,
+}
+
+impl From<SimulationResult> for PySimulationResult {
+    fn from(rust_result: SimulationResult) -> Self {
+        let mut py_state_updates = stdHashMap::new();
+        for (key, val) in rust_result.state_updates {
+            py_state_updates.insert(
+                Address::from(&key.to_fixed_bytes()).to_string(),
+                PyStateUpdate::from(val),
+            );
+        }
+        PySimulationResult {
+            result: rust_result.result.try_into()
+                .expect("Can't convert output bytes to a Python-compatible type"),
+            state_updates: py_state_updates,
+            gas_used: rust_result.gas_used,
+        }
+    }
 }
 
 fn get_runtime() -> Option<Arc<Runtime>> {
@@ -81,9 +133,7 @@ fn get_client() -> Arc<Provider<Http>> {
 }
 
 #[pyclass]
-pub struct WrappedSimulationEnginePy {
-    engine: SimulationEngine<Provider<Http>>,
-}
+pub struct WrappedSimulationEnginePy(SimulationEngine<Provider<Http>>);
 
 #[pymethods]
 impl WrappedSimulationEnginePy {
@@ -91,52 +141,13 @@ impl WrappedSimulationEnginePy {
     fn new() -> Self {
         let db = SimulationDB::new(get_client(), get_runtime(), None);
         let engine = SimulationEngine { state: db };
-        Self { engine }
+        Self(engine)
     }
 
     fn run_sim(self_: PyRef<Self>, params: PySimulationParameters) -> PyResult<PySimulationResult> {
-        let mut overrides = hash_map::HashMap::default();
-        println!("PySimulationParameters:");
-        println!("params.caller {:?}", params.caller);
-        println!("params.to {:?}", params.to);
-        println!("params.gas_limit {:?}", params.gas_limit);
-        println!("params.value {:?}", params.value);
-        if let Some(py_overrides) = params.overrides {
-            for (key, val) in py_overrides {
-                overrides.insert(
-                    U256::from_str(key.as_str()).unwrap(),
-                    U256::from_str(val.as_str()).unwrap(),
-                );
-            }
-        }
-        let parameter = simulation::SimulationParameters {
-            caller: Address::from_str(params.caller.as_str()).unwrap(),
-            to: Address::from_str(params.to.as_str()).unwrap(),
-            data: Bytes::from(params.data),
-            value: U256::from_str(params.value.as_str()).unwrap(),
-            overrides: Some(overrides),
-            gas_limit: params.gas_limit,
-        };
-
-        println!("simulation::SimulationParameters:");
-        println!("params.caller {:?}", parameter.caller);
-        println!("params.to {:?}", parameter.to);
-        println!("params.gas_limit {:?}", parameter.gas_limit);
-        println!("params.value {:?}", parameter.value);
-
-        let res = self_.engine.simulate(&parameter).unwrap();
-
-        let mut state_updated = HashMap::default();
-        for (key, val) in res.state_updates {
-            state_updated.insert(
-                Address::from(&key.to_fixed_bytes()).to_string(),
-                PyStateUpdate::new(val),
-            );
-        }
-        Ok(PySimulationResult {
-            result: res.result.try_into().unwrap(),
-            state_updates: state_updated,
-            gas_used: res.gas_used,
-        })
+        let res = self_.0
+            .simulate(&SimulationParameters::from(params))
+            .unwrap();  // TODO return error
+        Ok(PySimulationResult::from(res))
     }
 }
