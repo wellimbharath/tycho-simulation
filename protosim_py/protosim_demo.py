2c7e8328240947ee63ef1c255bd9da56355612dd
Michał D <dankiewicz.michal@gmail.com>
2023-06-07 13:31:14 +0200

    Fix Python demo
    Previously we initialised the very same account
    which we then attempted to call. This resulted in
    a simulation error because we didn't set correct
    contract code during account initialisation.
    
    Now the initialisation/update/cache clear don't
    have any effect on the simulation (they even happen
    afterward), but the point is to just show how to
    do it.
    
    --- don't change below this line ---
    ROC-1158 #time 38m

--- protosim_py/protosim_demo.py
+++ protosim_py/protosim_demo.py
@@ -18,51 +18,7 @@ U256MAX = 1157920892373161954235709850086879078532699846656405640394575840079131
 
 def test():
     print("Run test function")
-    sim = SimulationEngine()
-
-    acc_info = AccountInfo(
-        balance=U256MAX,
-        nonce=20,
-        code_hash="0xc5d2460186f7233c927e7db2dcc703c0e500b653ca82273b7bfad8045d85a470",
-        # fmt: off
-        code=bytearray([
-            208, 108, 166, 31, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
-            0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 5, 245, 225, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
-            0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 64, 0,
-            0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
-            0, 0, 0, 0, 0, 2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 160, 184, 105, 145,
-            198, 33, 139, 54, 193, 209, 157, 74, 46, 158, 176, 206, 54, 6, 235, 72, 0,
-            0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 192, 42, 170, 57, 178, 35, 254, 141, 10,
-            14, 92, 79, 39, 234, 217, 8, 60, 117, 108, 194
-        ]),
-        # fmt: on
-    )
-    perm_storage = {500: 500000, 20: 2000}
-    print("Inserting Account")
-    sim.init_account(
-        address="0xb4e16d0168e52d35cacd2c6185b44281ec28c9dc",
-        account=acc_info,
-        mocked=False,
-        permanent_storage=perm_storage,
-    )
-
-    print("Clear temp storage")
-    sim.clear_temp_storage()
-
-    bh = BlockHeader(
-        number=50,
-        hash="0xc5d2460186f7233c927e7db2dcc703c0e500b653ca82273b7bfad8045d85a470",
-        timestamp=200,
-    )
-
-    print("Attempting update")
-    update = {
-        "0xb4e16d0168e52d35cacd2c6185b44281ec28c9dc": StateUpdate(
-            balance=U256MAX, storage={U256MAX: U256MAX, 500: U256MAX}
-        )
-    }
-    sim.update_state(updates=update, block=bh)
-
+    engine = SimulationEngine()
     params = SimulationParameters(
         caller="0x0000000000000000000000000000000000000000",
         to="0x7a250d5630B4cF539739dF2C5dAcb4c659F2488D",
@@ -83,9 +39,42 @@ def test():
         gas_limit=500000000000000,
     )
     print("Run test sim")
-    res = sim.run_sim(params)
+    res = engine.run_sim(params)
     print("Sim done")
     print(f"{res.result=}")
+
+    # Demonstrate manually inserting and updating an account
+
+    print("Inserting Account")
+    engine.init_account(
+        address="0x95222290DD7278Aa3Ddd389Cc1E1d165CC4BAfe5",
+        account=AccountInfo(
+            balance=U256MAX,
+            nonce=20,
+            code_hash="0xc5d2460186f7233c927e7db2dcc703c0e500b653ca82273b7bfad8045d85a470",
+            code=None,
+        ),
+        mocked=False,  # i.e. missing storage will be queried from a node
+        permanent_storage={500: 500000, 20: 2000},
+    )
+
+    print("Clearing temp storage")
+    engine.clear_temp_storage()
+
+    print("Updating a manually-initialised account")
+    engine.update_state(
+        updates={
+            "0x95222290DD7278Aa3Ddd389Cc1E1d165CC4BAfe5": StateUpdate(
+                balance=U256MAX, storage={U256MAX: U256MAX, 500: U256MAX}
+            )
+        },
+        block=BlockHeader(
+            number=50,
+            hash="0xc5d2460186f7233c927e7db2dcc703c0e500b653ca82273b7bfad8045d85a470",
+            timestamp=200,
+        )
+    )
+
 
 
 if __name__ == "__main__":
