20e5a23677888b3aa5893817d538a5a666f52200
Michał D <dankiewicz.michal@gmail.com>
2023-06-23 13:28:05 +0200

    Change permissions of created wheels
    --- don't change below this line ---
    ROC-1194 #time 14m

--- protosim_py/build_protosim_wheel.sh
+++ protosim_py/build_protosim_wheel.sh
@@ -1,4 +1,11 @@
 #!/usr/bin/env sh
-# Should be executed from a repo root
+# USAGE:
+# sudo ./protosim_py/build_protosim_wheel.sh
+#
+# Should be executed from a repo root.
+# The last command must be executed as a root user, that's why we need sudo. The wheel produced by docker image
+# is owned by a root user, so we must change its permissions to use it. 
+
 docker build -t protosim_py_build -f protosim_py/protosim_py.Dockerfile .
 docker run -v .:/prop-builder protosim_py_build
+chmod -R 777 ./protosim_py/target/wheels/
