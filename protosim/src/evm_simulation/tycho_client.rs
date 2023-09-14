fe2ddae67c5a9ea9149ef22b4e4fecb470036ff3
Florian Pellissier <111426680+flopell@users.noreply.github.com>
2023-09-14 11:40:18 +0200

    Implement get_state

--- protosim/src/evm_simulation/tycho_client.rs
+++ protosim/src/evm_simulation/tycho_client.rs
@@ -1,3 +1,18 @@
+use chrono::NaiveDateTime;
+use futures::StreamExt;
+use hyper::{client::HttpConnector, Body, Client, Request, Uri};
+use log::{error, info};
+use serde::{Deserialize, Serialize};
+use std::collections::HashMap;
+use thiserror::Error;
+
+use super::tycho_models::Chain;
+use super::tycho_models::{Block, BlockStateChanges};
+use revm::primitives::{B160, B256, U256 as rU256};
+use tokio::sync::mpsc::{self, Receiver};
+use tokio_tungstenite::connect_async;
+use tokio_tungstenite::tungstenite::protocol::Message;
+
 #[derive(Error, Debug)]
 pub enum TychoClientError {
     #[error("Failed to parse URI: {0}. Error: {1}")]
@@ -9,6 +24,71 @@ pub enum TychoClientError {
     #[error("Failed to parse response: {0}")]
     ParseResponse(String),
 }
+#[derive(Serialize, Debug)]
+pub struct StateRequestBody {
+    contract_ids: Option<Vec<B160>>,
+    version: Option<Version>,
+}
+
+impl StateRequestBody {
+    pub fn from_block(block: Block) -> Self {
+        Self {
+            contract_ids: None,
+            version: Some(Version {
+                timestamp: None,
+                block: Some(RequestBlock {
+                    hash: block.hash,
+                    number: block.number,
+                    chain: block.chain,
+                }),
+            }),
+        }
+    }
+}
+#[derive(Serialize, Debug)]
+pub struct Version {
+    timestamp: Option<NaiveDateTime>,
+    block: Option<RequestBlock>,
+}
+
+#[derive(Serialize, Debug)]
+struct RequestBlock {
+    hash: B256,
+    number: u64,
+    chain: Chain,
+}
+#[derive(Default)]
+pub struct GetStateFilters {
+    tvl_gt: Option<u64>,
+    intertia_min_gt: Option<u64>,
+}
+
+impl GetStateFilters {
+    fn to_query_string(&self) -> String {
+        let mut parts = Vec::new();
+
+        parts.push(format!("chain={}", Chain::Ethereum.to_string()));
+
+        if let Some(tvl_gt) = self.tvl_gt {
+            parts.push(format!("tvl_gt={}", tvl_gt));
+        }
+
+        if let Some(intertia_min_gt) = self.intertia_min_gt {
+            parts.push(format!("intertia_min_gt={}", intertia_min_gt));
+        }
+
+        parts.join("&")
+    }
+}
+#[derive(Deserialize)]
+pub struct ResponseAccount {
+    pub address: B160,
+    pub slots: HashMap<rU256, rU256>,
+    pub balance: rU256,
+    pub code: Vec<u8>,
+    pub code_hash: B256,
+}
+
 pub struct TychoVmStateClient {
     http_client: Client<HttpConnector>,
     base_uri: Uri,
@@ -25,4 +105,44 @@ impl TychoVmStateClient {
             http_client: Client::new(),
             base_uri,
         })
+    }
+
+    pub async fn get_state(
+        &self,
+        filters: Option<&GetStateFilters>,
+        request: Option<&StateRequestBody>,
+    ) -> Result<Vec<ResponseAccount>, TychoClientError> {
+        let mut url = format!("{}/{}", self.base_uri, "contract_state");
+        let mut body = Body::empty();
+
+        if let Some(filters) = filters {
+            let query_string = filters.to_query_string();
+            if !query_string.is_empty() {
+                url = format!("{}?{}", url, query_string);
+            }
+        }
+        if let Some(request) = request {
+            if request.to_owned().contract_ids.is_some() || request.to_owned().version.is_some() {
+                let serialized = serde_json::to_string(&request).unwrap();
+                body = Body::from(serialized);
+            }
+        }
+
+        let req = Request::get(url)
+            .body(body)
+            .map_err(|e| TychoClientError::FormatRequest(e.to_string()))?;
+
+        let response = self
+            .http_client
+            .request(req)
+            .await
+            .map_err(|e| TychoClientError::HttpClient(e.to_string()))?;
+
+        let body = hyper::body::to_bytes(response.into_body())
+            .await
+            .map_err(|e| TychoClientError::ParseResponse(e.to_string()))?;
+        let accounts: Vec<ResponseAccount> = serde_json::from_slice(&body)
+            .map_err(|e| TychoClientError::ParseResponse(e.to_string()))?;
+
+        Ok(accounts)
     }
