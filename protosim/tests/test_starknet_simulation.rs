e4b51d6404012ec00de057e2f2a7db0b4306ae19
Louise Poole <louise@datarevenue.com>
2023-11-02 14:20:53 +0200

    Update felt252 converters

--- protosim/tests/test_starknet_simulation.rs
+++ protosim/tests/test_starknet_simulation.rs
@@ -2,7 +2,6 @@
 mod tests {
     use cairo_vm::felt::Felt252;
     use dotenv::dotenv;
-    use num_traits::Num;
     use protosim::starknet_simulation::{
         rpc_reader::RpcStateReader,
         simulation::{ContractOverride, SimulationEngine, SimulationParameters},
@@ -12,8 +11,15 @@ mod tests {
     use starknet_in_rust::utils::{felt_to_hash, get_storage_var_address, Address, ClassHash};
     use std::{collections::HashMap, env, sync::Arc};
 
-    fn string_to_address(address: &str) -> Address {
-        Address(Felt252::from_str_radix(address, 16).expect("hex address"))
+    pub fn felt_str(val: &str) -> Felt252 {
+        let base = if val.starts_with("0x") { 16_u32 } else { 10_u32 };
+        let stripped_val = val.strip_prefix("0x").unwrap_or(val);
+
+        Felt252::parse_bytes(stripped_val.as_bytes(), base).expect("Failed to parse input")
+    }
+
+    pub fn address_str(val: &str) -> Address {
+        Address(felt_str(val))
     }
 
     fn setup_reader(block_number: u64) -> RpcStateReader {
@@ -75,18 +81,19 @@ mod tests {
     }
 
     #[test]
+    #[cfg_attr(not(feature = "network_tests"), ignore)]
     fn test_consecutive_simulations_ekubo() {
         // Test vars
         let block_number = 354168;
         let token0 =
-            string_to_address("da114221cb83fa859dbdb4c44beeaa0bb37c7537ad5ae66fe5e0efd20e6eb3");
+            address_str("0xda114221cb83fa859dbdb4c44beeaa0bb37c7537ad5ae66fe5e0efd20e6eb3");
         let token1 =
-            string_to_address("068f5c6a61780768455de69077e07e89787839bf8166decfbf92b645209c0fb8");
+            address_str("0x068f5c6a61780768455de69077e07e89787839bf8166decfbf92b645209c0fb8");
         let test_wallet =
-            string_to_address("065c19e14e2587d2de74c561b2113446ca4b389aabe6da1dc4accb6404599e99");
+            address_str("0x065c19e14e2587d2de74c561b2113446ca4b389aabe6da1dc4accb6404599e99");
         let ekubo_address =
-            string_to_address("00000005dd3d2f4429af886cd1a3b08289dbcea99a294197e9eb43b0e0325b4b");
-        let sell_amount = Felt252::from_str_radix("3bf9da25c1bfd31da", 16).unwrap();
+            address_str("0x00000005dd3d2f4429af886cd1a3b08289dbcea99a294197e9eb43b0e0325b4b");
+        let sell_amount = felt_str("0x3bf9da25c1bfd31da");
 
         // Contruct engine with sell token override
         let contract_overrides = construct_token_overrides(
@@ -100,18 +107,18 @@ mod tests {
         // obtained from this Ekubo core swap call: https://voyager.online/tx/0x634fa25f6b3fb6aceffbf689edb04eb24d4eb118a955d3439382a231e78b7e7#internalCalls
         let swap_calldata = vec![
             // Pool key data
-            token0.0,                                                              // token0
-            token1.0,                                                              // token1
-            Felt252::from_str_radix("5e59d28446cbf2061e33040400000", 16).unwrap(), // fee
-            Felt252::from(10),                                                     // tick spacing
-            Felt252::from(0),                                                      // extension
+            token0.0,                                    // token0
+            token1.0,                                    // token1
+            felt_str("0x5e59d28446cbf2061e33040400000"), // fee
+            Felt252::from(10),                           // tick spacing
+            Felt252::from(0),                            // extension
             // Swap data
-            sell_amount,      // amount
-            Felt252::from(0), // amount sign
-            Felt252::from(0), // istoken1
-            Felt252::from_str_radix("10c6cdcb20b7a5db24ca0ceb6980", 16).unwrap(), // sqrt ratio limit
-            Felt252::from(0),
-            Felt252::from(100), // skip ahead
+            sell_amount,                                // amount
+            Felt252::from(0),                           // amount sign
+            Felt252::from(0),                           // istoken1
+            felt_str("0x10c6cdcb20b7a5db24ca0ceb6980"), // sqrt ratio limit (lower bits
+            Felt252::from(0),                           // sqrt ratio limit (upper bits)
+            Felt252::from(100),                         // skip ahead
         ];
 
         let params = SimulationParameters::new(
