ed0d6faf4a2b5f181fccdcaaf0104aa52485ea18
Louise Poole <louise@datarevenue.com>
2023-11-02 11:41:47 +0200

    Fix swap data

--- protosim/tests/test_starknet_simulation.rs
+++ protosim/tests/test_starknet_simulation.rs
@@ -80,18 +80,18 @@ mod tests {
         let block_number = 354168;
         let token0 =
             string_to_address("da114221cb83fa859dbdb4c44beeaa0bb37c7537ad5ae66fe5e0efd20e6eb3");
-        // let token1 =
-        //     string_to_address("068f5c6a61780768455de69077e07e89787839bf8166decfbf92b645209c0fb8");
+        let token1 =
+            string_to_address("068f5c6a61780768455de69077e07e89787839bf8166decfbf92b645209c0fb8");
         let test_wallet =
             string_to_address("065c19e14e2587d2de74c561b2113446ca4b389aabe6da1dc4accb6404599e99");
         let ekubo_address =
             string_to_address("00000005dd3d2f4429af886cd1a3b08289dbcea99a294197e9eb43b0e0325b4b");
-        let sell_amount = Felt252::from_str_radix("69147602770206732762", 16).unwrap();
+        let sell_amount = Felt252::from_str_radix("3bf9da25c1bfd31da", 16).unwrap();
 
         // Contruct engine with sell token override
         let contract_overrides = construct_token_overrides(
             test_wallet.clone(),
-            token0,
+            token0.clone(),
             sell_amount.clone(),
             ekubo_address.clone(),
         );
@@ -100,24 +100,16 @@ mod tests {
         // obtained from this Ekubo core swap call: https://voyager.online/tx/0x634fa25f6b3fb6aceffbf689edb04eb24d4eb118a955d3439382a231e78b7e7#internalCalls
         let swap_calldata = vec![
             // Pool key data
-            Felt252::from_str_radix(
-                "385291772725090318157700937045086145273563247402457518748197066808155336371",
-                16,
-            )
-            .unwrap(), // token0
-            Felt252::from_str_radix(
-                "2967174050445828070862061291903957281356339325911846264948421066253307482040",
-                16,
-            )
-            .unwrap(), // token1
-            Felt252::from_str_radix("30618607375546000000000000000000000", 16).unwrap(), // fee
-            Felt252::from(10), // tick spacing
-            Felt252::from(0),  // extension
+            token0.0,                                                              // token0
+            token1.0,                                                              // token1
+            Felt252::from_str_radix("5e59d28446cbf2061e33040400000", 16).unwrap(), // fee
+            Felt252::from(10),                                                     // tick spacing
+            Felt252::from(0),                                                      // extension
             // Swap data
             sell_amount,      // amount
             Felt252::from(0), // amount sign
             Felt252::from(0), // istoken1
-            Felt252::from_str_radix("340269419829709255939292639488384", 16).unwrap(), // sqrt ratio limit
+            Felt252::from_str_radix("10c6cdcb20b7a5db24ca0ceb6980", 16).unwrap(), // sqrt ratio limit
             Felt252::from(0),
             Felt252::from(100), // skip ahead
         ];
