418b78721b29729214b9ee307aec45f71f12e0d5
Louise Poole <louise@datarevenue.com>
2023-11-02 11:06:39 +0200

    Add token overrides

--- protosim/tests/test_starknet_simulation.rs
+++ protosim/tests/test_starknet_simulation.rs
@@ -9,20 +9,19 @@ mod tests {
     };
     use rpc_state_reader::rpc_state::{RpcChain, RpcState};
     use starknet_api::block::BlockNumber;
-    use starknet_in_rust::utils::{Address, ClassHash};
-    use std::{env, sync::Arc};
+    use starknet_in_rust::utils::{felt_to_hash, get_storage_var_address, Address, ClassHash};
+    use std::{collections::HashMap, env, sync::Arc};
 
     fn string_to_address(address: &str) -> Address {
         Address(Felt252::from_str_radix(address, 16).expect("hex address"))
     }
 
     fn setup_reader(block_number: u64) -> RpcStateReader {
-        dotenv().expect("Missing .env file");
-        let rpc_endpoint = format!(
-            "https://{}.infura.io/v3/{}",
-            RpcChain::MainNet,
-            env::var("INFURA_API_KEY").expect("missing infura api key")
-        );
+        let infura_api_key = env::var("INFURA_API_KEY").unwrap_or_else(|_| {
+            dotenv().expect("Missing .env file");
+            env::var("INFURA_API_KEY").expect("Missing INFURA_API_KEY in .env file")
+        });
+        let rpc_endpoint = format!("https://{}.infura.io/v3/{}", RpcChain::MainNet, infura_api_key);
         let feeder_url = format!("https://{}.starknet.io/feeder_gateway", RpcChain::MainNet);
         RpcStateReader::new(RpcState::new(
             RpcChain::MainNet,
@@ -34,43 +33,69 @@ mod tests {
 
     fn setup_engine(
         block_number: u64,
-        token0: Address,
-        token1: Address,
+        contract_overrides: Option<Vec<ContractOverride>>,
     ) -> SimulationEngine<RpcStateReader> {
         let rpc_state_reader = Arc::new(setup_reader(block_number));
-
-        // // Ekubo contract
-        // let address =
-        //     string_to_address("00000005dd3d2f4429af886cd1a3b08289dbcea99a294197e9eb43b0e0325b4b");
-        // let class_hash: ClassHash =
-        //     hex::decode("008e44da7924f4807d4200f8ff9938182e0bd6841dcb62ef397fcfd7fb2ff1e0")
-        //         .unwrap()
-        //         .as_slice()
-        //         .try_into()
-        //         .unwrap();
-        // let abi_path = "tests/resources/ekubo.json";
-        // let ekubo_contract =
-        //     ContractOverride::new(address, class_hash, Some(abi_path.to_owned()), None);
-
-        // ERC20 contract overrides
-
-        let contract_overrides = vec![];
+        let contract_overrides = contract_overrides.unwrap_or_else(Vec::new);
 
         SimulationEngine::new(rpc_state_reader, contract_overrides).unwrap()
     }
 
+    fn construct_token_overrides(
+        wallet: Address,
+        sell_token: Address,
+        sell_amount: Felt252,
+        spender: Address,
+    ) -> Vec<ContractOverride> {
+        // ERC20 contract overrides - using USDC token contract template
+        let class_hash: ClassHash =
+            hex::decode("052c7ba99c77fc38dd3346beea6c0753c3471f2e3135af5bb837d6c9523fff62")
+                .unwrap()
+                .as_slice()
+                .try_into()
+                .unwrap();
+
+        let mut storage_overrides = HashMap::new();
+
+        // override balance
+        let balance_storage_hash =
+            felt_to_hash(&get_storage_var_address("ERC20_balances", &[wallet.0.clone()]).unwrap());
+        storage_overrides.insert((sell_token.clone(), balance_storage_hash), sell_amount.clone());
+
+        // override allowance
+        let allowance_storage_hash = felt_to_hash(
+            &get_storage_var_address("ERC20_allowances", &[wallet.0, spender.0]).unwrap(),
+        );
+        storage_overrides.insert((sell_token.clone(), allowance_storage_hash), sell_amount);
+
+        let token_contract =
+            ContractOverride::new(sell_token, class_hash, None, Some(storage_overrides));
+
+        vec![token_contract]
+    }
+
     #[test]
     fn test_consecutive_simulations_ekubo() {
-        // create engine with mocked erc20 token contracts
+        // Test vars
         let block_number = 354168;
         let token0 =
             string_to_address("da114221cb83fa859dbdb4c44beeaa0bb37c7537ad5ae66fe5e0efd20e6eb3");
-        let token1 =
-            string_to_address("068f5c6a61780768455de69077e07e89787839bf8166decfbf92b645209c0fb8");
-        let mut engine = setup_engine(block_number, token0, token1);
-
+        // let token1 =
+        //     string_to_address("068f5c6a61780768455de69077e07e89787839bf8166decfbf92b645209c0fb8");
+        let test_wallet =
+            string_to_address("065c19e14e2587d2de74c561b2113446ca4b389aabe6da1dc4accb6404599e99");
         let ekubo_address =
             string_to_address("00000005dd3d2f4429af886cd1a3b08289dbcea99a294197e9eb43b0e0325b4b");
+        let sell_amount = Felt252::from_str_radix("69147602770206732762", 16).unwrap();
+
+        // Contruct engine with sell token override
+        let contract_overrides = construct_token_overrides(
+            test_wallet.clone(),
+            token0,
+            sell_amount.clone(),
+            ekubo_address.clone(),
+        );
+        let mut engine = setup_engine(block_number, Some(contract_overrides));
 
         // obtained from this Ekubo core swap call: https://voyager.online/tx/0x634fa25f6b3fb6aceffbf689edb04eb24d4eb118a955d3439382a231e78b7e7#internalCalls
         let swap_calldata = vec![
@@ -89,16 +114,16 @@ mod tests {
             Felt252::from(10), // tick spacing
             Felt252::from(0),  // extension
             // Swap data
-            Felt252::from_str_radix("69147602770206732762", 16).unwrap(), // amount
-            Felt252::from(0),                                             // amount sign
-            Felt252::from(0),                                             // istoken1
+            sell_amount,      // amount
+            Felt252::from(0), // amount sign
+            Felt252::from(0), // istoken1
             Felt252::from_str_radix("340269419829709255939292639488384", 16).unwrap(), // sqrt ratio limit
             Felt252::from(0),
             Felt252::from(100), // skip ahead
         ];
 
         let params = SimulationParameters::new(
-            string_to_address("065c19e14e2587d2de74c561b2113446ca4b389aabe6da1dc4accb6404599e99"), // caller used in other tests
+            test_wallet,
             ekubo_address,
             swap_calldata,
             "swap".to_owned(),
