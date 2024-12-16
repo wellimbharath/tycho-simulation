5ffb440670fd310769c51cc3b5ebdcc2d81f3d81
Diana Carvalho <diana@propellerheads.xyz>
2024-12-16 11:41:08 +0000

    feat: Update tycho dependencies tag and re export them

--- src/lib.rs
+++ src/lib.rs
@@ -5,20 +5,14 @@
 //! It allows to simulate chained trades over different venues
 //! together to exploit price differences by using token prices
 //! calculated from the protocol's state.
-//!
-//! The main data structure is a graph which allows to search
-//! for a sequence of swaps that provide some desired outcome,
-//! such as atomic arbitrage opportunities. The graph models
-//! each token as a node and decentralised exchange protocols
-//! as edges.
-//!
-//! The crate also provides optimization methods, such as golden
-//! section search, to find optimal amounts for a specific sequence.
 
 extern crate core;
 
 // Reexports
-pub use num_traits;
+pub use tycho_client;
+pub use tycho_core;
+pub use tycho_ethereum;
+
 #[cfg(feature = "evm")]
 pub mod evm;
 pub mod models;
