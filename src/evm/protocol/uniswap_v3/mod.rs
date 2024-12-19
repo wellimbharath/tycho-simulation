0d02a1e76f64dd94d063e4dc6d97bc45a8fada0a
zizou <111426680+flopell@users.noreply.github.com>
2024-12-19 16:16:37 +0100

    refactor(protocol): Move more uniswap_v3 functions into `utils/uniswap`
    This is because these functions will also be used by Uniswap V4.

--- src/evm/protocol/uniswap_v3/mod.rs
+++ src/evm/protocol/uniswap_v3/mod.rs
@@ -1,8 +1,4 @@
 //! Uniswap V3 Decentralized Exchange
 pub mod enums;
-mod liquidity_math;
-mod solidity_math;
-mod sqrt_price_math;
 pub mod state;
-mod swap_math;
 pub mod tycho_decoder;
--- src/evm/protocol/utils/uniswap/mod.rs
+++ src/evm/protocol/utils/uniswap/mod.rs
@@ -1,2 +1,37 @@
+use alloy_primitives::{I256, U256};
+
+pub(crate) mod liquidity_math;
+mod solidity_math;
+pub(crate) mod sqrt_price_math;
+pub(crate) mod swap_math;
 pub(crate) mod tick_list;
 pub(crate) mod tick_math;
+
+#[derive(Debug)]
+pub(crate) struct SwapState {
+    pub(crate) amount_remaining: I256,
+    pub(crate) amount_calculated: I256,
+    pub(crate) sqrt_price: U256,
+    pub(crate) tick: i32,
+    pub(crate) liquidity: u128,
+}
+
+#[derive(Debug)]
+pub(crate) struct StepComputation {
+    pub(crate) sqrt_price_start: U256,
+    pub(crate) tick_next: i32,
+    pub(crate) initialized: bool,
+    pub(crate) sqrt_price_next: U256,
+    pub(crate) amount_in: U256,
+    pub(crate) amount_out: U256,
+    pub(crate) fee_amount: U256,
+}
+
+#[derive(Debug)]
+pub(crate) struct SwapResults {
+    pub(crate) amount_calculated: I256,
+    pub(crate) sqrt_price: U256,
+    pub(crate) liquidity: u128,
+    pub(crate) tick: i32,
+    pub(crate) gas_used: U256,
+}
