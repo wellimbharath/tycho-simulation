fd7e2b4f0eeeb3806d964c6fd90178cad99dd08f
zizou <111426680+flopell@users.noreply.github.com>
2024-12-19 14:04:17 +0100

    refactor(protocol): Move uniswapV3 math files into a utils/uniswap module
    This is because this logic will be also needed by uniswap V4. Therefore we decided to introduce a module that would be shared between these two protocols. We will move more things in the next PRs.

--- src/evm/protocol/uniswap_v3/mod.rs
+++ src/evm/protocol/uniswap_v3/mod.rs
@@ -5,6 +5,4 @@ mod solidity_math;
 mod sqrt_price_math;
 pub mod state;
 mod swap_math;
-pub mod tick_list;
-mod tick_math;
 pub mod tycho_decoder;
---
+++ src/evm/protocol/utils/uniswap/mod.rs
@@ -0,0 +1,2 @@
+pub(crate) mod tick_list;
+pub(crate) mod tick_math;
